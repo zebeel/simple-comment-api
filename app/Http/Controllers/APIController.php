c0ccc1dc839dd69e9bafc7ea8fcb33e7d16f9791
Lee Vaz <chuong.le.jp@gmail.com>
2020-11-16 17:30:57 +0900

    first commit

---
+++ app/Http/Controllers/APIController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class APIController extends Controller
+{
+    public function getAllComments(Request $request) {
+        return Comment::all();
+    }
+
+    public function newComment(Request $request) {
+        $data = $request->all();
+        $validator = Validator::make($data, [
+            'by' => 'required',
+            'content' => 'required|min:5|max:3000',
+        ]);
+        if($validator->fails()) {
+            return response(['error' => $validator->errors()]);
+        }
+        $comment = Comment::create($data);
+
+        return response(['comment' => $comment], 200);
+    }
+}
