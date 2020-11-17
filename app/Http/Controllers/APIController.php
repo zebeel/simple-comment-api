a8647a250c2a5d509dfc80716cd92a08a7b2b446
Lee Vaz <chuong.le.jp@gmail.com>
2020-11-17 18:06:17 +0900

    fix api

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -4,12 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Models\Comment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class APIController extends Controller
 {
     public function getAllComments(Request $request) {
-        return Comment::all();
+        $data = $request->all();
+        $validator = Validator::make($data, [
+            'to' => 'required',
+        ]);
+        if($validator->fails()) {
+            return response(['error' => $validator->errors(), 'count' => 0]);
+        }
+
+        $comments = Comment::all();
+        foreach ($comments as $comment) {
+            $comment->clock = date_format($comment->created_at,"M d, Y \\a\\t h:ia");
+            $comment->avatar = "https://ui-avatars.com/api/?background=random&name=$comment->by";
+        }
+
+        return response(['comments' => $comments, 'count' => count($comments)]);
     }
 
     public function newComment(Request $request) {
@@ -23,6 +38,9 @@ class APIController extends Controller
             return response(['error' => $validator->errors()]);
         }
         $comment = Comment::create($data);
+
+        $comment->clock = date_format($comment->created_at,"M d, Y \\a\\t h:ia");
+        $comment->avatar = "https://ui-avatars.com/api/?background=random&name=$comment->by";
 
         return response(['comment' => $comment], 200);
     }
